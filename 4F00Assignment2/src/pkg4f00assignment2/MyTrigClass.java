7aa363c6a4e67a1aa6afd2d9178a7611992288dc
Param Jansari <paramjansari0@gmail.com>
2019-11-11 15:52:03 -0500

    Version 1 - All Tests Failed

---
+++ 4F00Assignment2/src/pkg4f00assignment2/MyTrigClass.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg4f00assignment2;
+
+/**
+ *
+ * @author Param
+ */
+public class MyTrigClass {
+    public static final double PI = 3.141592653589793;
+    
+    public static double sin(double x) {
+        return 0;
+    }
+
+    public static double cos(double x) {
+        return 0;
+    }
+
+    public static double tan(double x) {
+        return 0;
+    }
+
+    public static double cot(double x) {
+        return 0;
+    }
+    
+    public static double sec(double x) {
+        return 0;
+    }
+
+    public static double csc(double x) {
+        return 0;
+    }
+
+    public static double toRadians(double degrees) {
+        return 0;
+    }
+
+    public static double toDegrees(double radians) {
+        return 0;
+    }
+    
+    public static double sqrt(double number){
+        return 0;
+    }
+    
+    public static double power(double base, int exp){
+        return 0;
+    }
+    
+    public static double factorial(double number){
+        return 0;
+    }
+}
